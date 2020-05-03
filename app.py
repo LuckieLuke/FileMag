042e52416cdd01534d6f64c82666a337d2ced7f0
LuckieLuke <luk19984@gmail.com>
2020-05-04 00:24:22 +0200

    few new features

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
+
+@app.route("/list/")
+def listFiles():
+    path = "FILES"
+    files = os.listdir(path)
+    dirs = next(os.walk('./FILES'))[1]
+
+    for directory in dirs:
+        if directory in files:
+            files.remove(directory)
+
+    return render_template("index.html", files=files, dirs=dirs)
 
 if __name__ == "__main__":
     app.run(debug=True)
