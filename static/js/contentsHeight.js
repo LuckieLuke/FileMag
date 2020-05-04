f973db78a862d53e892c0633711d7cca0a45a246
LuckieLuke <luk19984@gmail.com>
2020-05-04 22:51:24 +0200

    fixing resizing

--- static/js/contentsHeight.js
+++ static/js/contentsHeight.js
@@ -15,8 +15,31 @@ const setContentsSizes = () => {
     }
 };
 
-window.addEventListener("resize", () => {
-    setContentsSizes();
-})
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    window.addEventListener("resize", () => {
+        setContentsSizes();
+    })
+}
 
 setContentsSizes();
